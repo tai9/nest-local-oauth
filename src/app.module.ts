d1971b792e66e6a9deb9fd2f22a8465293071a48
Tai <tantai.it95@gmail.com>
2023-09-24 16:50:21 +0700

    feat: implement common services

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { validationSchema } from './config/config.schema';
+import { config } from './config';
+import { MikroOrmModule } from '@mikro-orm/nestjs';
+import { MikroOrmConfig } from './config/mikroorm.config';
+import { CommonModule } from './common/common.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema,
+      load: [config],
+    }),
+    MikroOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useClass: MikroOrmConfig,
+    }),
+    CommonModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
